e5eef085e5b5055ccfc6a47d30ba3782bd68ba1a
kbouth <kbouthsar1@bnl.gov>
2025-08-22 15:12:35 -0400

    updated ip sources

--- src/hw/tcl/main.tcl
+++ src/hw/tcl/main.tcl
@@ -17,67 +17,11 @@ proc setSources {} {
   variable Sources 
 
   
-  lappend Sources {"../hdl/top_tb.vhd" "VHDL 2008"}
   lappend Sources {"../hdl/top.vhd" "VHDL 2008"} 
   lappend Sources {"../hdl/acmi_package.vhd" "VHDL 2008"} 
-  lappend Sources {"../hdl/pwronreset.vhd" "VHDL 2008"} 
-  lappend Sources {"../hdl/debounce.vhd" "VHDL 2008"} 
-  lappend Sources {"../hdl/faults.vhd" "VHDL 2008"} 
-  
-  lappend Sources {"../hdl/gen_samplenum.vhd" "VHDL 2008"} 
-  lappend Sources {"../hdl/gen_startup_dly.vhd" "VHDL 2008"} 
-  lappend Sources {"../hdl/gen_test_pulses.vhd" "VHDL 2008"}  
-  lappend Sources {"../hdl/gen_timestamp.vhd" "VHDL 2008"}  
-  lappend Sources {"../hdl/gen_timing_events.vhd" "VHDL 2008"}     
-  #lappend Sources {"../hdl/gen_trig_pulse.vhd" "VHDL 2008"}   
-  lappend Sources {"../hdl/gen_window.vhd" "VHDL 2008"}   
-  lappend Sources {"../hdl/beam_detect_window.vhd" "VHDL 2008"}  
-  lappend Sources {"../hdl/pulse_gen.vhd" "VHDL 2008"}   
-  
-  lappend Sources {"../hdl/calc_charge.vhd" "VHDL 2008"} 
-  lappend Sources {"../hdl/calc_beam_stats.vhd" "VHDL 2008"}   
-  lappend Sources {"../hdl/calc_baseline.vhd" "VHDL 2008"}  
-  lappend Sources {"../hdl/calc_fwhm.vhd" "VHDL 2008"}   
-  lappend Sources {"../hdl/calc_integral.vhd" "VHDL 2008"} 
-  lappend Sources {"../hdl/find_peak.vhd" "VHDL 2008"} 
-  lappend Sources {"../hdl/gen_gates.vhd" "VHDL 2008"}   
-  lappend Sources {"../hdl/find_beam_oow.vhd" "VHDL 2008"}    
-  lappend Sources {"../hdl/gen_gate.vhd" "VHDL 2008"} 
-  
-  lappend Sources {"../hdl/accumulator.vhd" "VHDL 2008"}  
-  
-  lappend Sources {"../hdl/i2c_monitors.vhd" "VHDL 2008"}
-  
-  lappend Sources {"../hdl/rx_backend_data.vhd" "VHDL 2008"} 
-  lappend Sources {"../hdl/tx_backend_data.vhd" "VHDL 2008"}  
-  lappend Sources {"../hdl/find_boow_pulse.vhd" "VHDL 2008"}   
-  lappend Sources {"../hdl/beamoow_fifodata.vhd" "VHDL 2008"}   
-  lappend Sources {"../hdl/faults_tp.vhd" "VHDL 2008"} 
-  
-  lappend Sources {"../hdl/eeprom_interface.vhd" "VHDL 2008"} 
-  lappend Sources {"../hdl/eeprom_spi.vhd" "VHDL 2008"} 
-  lappend Sources {"../hdl/crc_compute.vhd" "VHDL 2008"}     
-  lappend Sources {"../hdl/crc32.vhd" "VHDL 2008"}   
-  
-  lappend Sources {"../hdl/adc_interface.vhd" "VHDL 2008"}   
-  lappend Sources {"../hdl/read_ltc2107.vhd" "VHDL 2008"} 
-  lappend Sources {"../hdl/ltc2107_spi.vhd" "VHDL 2008"}  
-  
-  lappend Sources {"../hdl/kria_comm_wrapper.vhd" "VHDL 2008"}   
-  lappend Sources {"../hdl/align_rxdata.vhd" "VHDL 2008"} 
-  lappend Sources {"../hdl/gtp/kria_comm_support.vhd" "VHDL 2008"} 
-  lappend Sources {"../hdl/gtp/kria_comm_common_reset.vhd" "VHDL 2008"}     
-  lappend Sources {"../hdl/gtp/kria_comm_gt_usrclk_source.vhd" "VHDL 2008"}   
-  lappend Sources {"../hdl/gtp/kria_comm_clock_module.vhd" "VHDL 2008"}  
-  lappend Sources {"../hdl/gtp/kria_comm_cpll_railing.vhd" "VHDL 2008"}   
-  lappend Sources {"../hdl/gtp/kria_comm_common.vhd" "VHDL 2008"}        
-  
-  lappend Sources {"../hdl/stretch.vhd" "VHDL 2008"}  
 
   lappend Sources {"../cstr/pins.xdc"  "XDC"} 
-  lappend Sources {"../cstr/gtp.xdc"  "XDC"}   
-  lappend Sources {"../cstr/timing.xdc"  "XDC"}  
-  lappend Sources {"../cstr/debug.xdc"  "XDC"}   
+  lappend Sources {"../cstr/gtp.xdc"  "XDC"}     
   
   
 }

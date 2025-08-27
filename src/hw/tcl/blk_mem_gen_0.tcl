977d3371924e226959aacfe966e270b19ccf217e
kbouth <kbouthsar1@bnl.gov>
2025-08-27 14:36:08 -0400

    updated src dir and added ip tcl files

---
+++ src/hw/tcl/blk_mem_gen_0.tcl
@@ -0,0 +1,81 @@
+##################################################################
+# CHECK VIVADO VERSION
+##################################################################
+
+set scripts_vivado_version 2022.2
+set current_vivado_version [version -short]
+
+if { [string first $scripts_vivado_version $current_vivado_version] == -1 } {
+  catch {common::send_msg_id "IPS_TCL-100" "ERROR" "This script was generated using Vivado <$scripts_vivado_version> and is being run in <$current_vivado_version> of Vivado. Please run the script in Vivado <$scripts_vivado_version> then open the design in Vivado <$current_vivado_version>. Upgrade the design by running \"Tools => Report => Report IP Status...\", then run write_ip_tcl to create an updated script."}
+  return 1
+}
+
+##################################################################
+# START
+##################################################################
+
+# To test this script, run the following commands from Vivado Tcl console:
+# source blk_mem_gen_0.tcl
+# If there is no project opened, this script will create a
+# project, but make sure you do not have an existing project
+# in the current working folder.
+
+set list_projs [get_projects -quiet]
+if { $list_projs eq "" } {
+  create_project zubpm_hw zubpm_hw -part xc7a200tfbg484-2
+  set_property target_language VHDL [current_project]
+  set_property simulator_language Mixed [current_project]
+}
+
+##################################################################
+# CHECK IPs
+##################################################################
+
+set bCheckIPs 1
+set bCheckIPsPassed 1
+if { $bCheckIPs == 1 } {
+  set list_check_ips { xilinx.com:ip:blk_mem_gen:8.4 }
+  set list_ips_missing ""
+  common::send_msg_id "IPS_TCL-1001" "INFO" "Checking if the following IPs exist in the project's IP catalog: $list_check_ips ."
+
+  foreach ip_vlnv $list_check_ips {
+  set ip_obj [get_ipdefs -all $ip_vlnv]
+  if { $ip_obj eq "" } {
+    lappend list_ips_missing $ip_vlnv
+    }
+  }
+
+  if { $list_ips_missing ne "" } {
+    catch {common::send_msg_id "IPS_TCL-105" "ERROR" "The following IPs are not found in the IP Catalog:\n  $list_ips_missing\n\nResolution: Please add the repository containing the IP(s) to the project." }
+    set bCheckIPsPassed 0
+  }
+}
+
+if { $bCheckIPsPassed != 1 } {
+  common::send_msg_id "IPS_TCL-102" "WARNING" "Will not continue with creation of design due to the error(s) above."
+  return 1
+}
+
+##################################################################
+# CREATE IP blk_mem_gen_0
+##################################################################
+
+set blk_mem_gen_0 [create_ip -name blk_mem_gen -vendor xilinx.com -library ip -version 8.4 -module_name blk_mem_gen_0]
+
+# User Parameters
+set_property -dict [list \
+  CONFIG.Coe_File {/home/bouthsarath/acmi-chainb/prj/zubpm_hw/zubpm_hw.srcs/sources_1/imports/acmi-chainb/adc_data.coe} \
+  CONFIG.Enable_A {Always_Enabled} \
+  CONFIG.Fill_Remaining_Memory_Locations {false} \
+  CONFIG.Load_Init_File {true} \
+  CONFIG.Memory_Type {Single_Port_ROM} \
+  CONFIG.Write_Depth_A {16000} \
+] [get_ips blk_mem_gen_0]
+
+# Runtime Parameters
+set_property -dict { 
+  GENERATE_SYNTH_CHECKPOINT {0}
+} $blk_mem_gen_0
+
+##################################################################
+
--- src/hw/tcl/main.tcl
+++ src/hw/tcl/main.tcl
@@ -19,9 +19,37 @@ proc setSources {} {
   
   lappend Sources {"../hdl/top.vhd" "VHDL 2008"} 
   lappend Sources {"../hdl/acmi_package.vhd" "VHDL 2008"} 
+  lappend Sources {"../hdl/ACMI_tpg.vhd" "VHDL 2008"}
+  lappend Sources {"../hdl/adc_interface.vhd" "VHDL 2008"}
+  lappend Sources {"../hdl/adc_readout_test.vhd" "VHDL 2008"}
+  lappend Sources {"../hdl/backend_comm_wrapper.vhd" "VHDL 2008"}
+  lappend Sources {"../hdl/calc_baseline.vhd" "VHDL 2008"}
+  lappend Sources {"../hdl/calc_charge.vhd" "VHDL 2008"}    
+  lappend Sources {"../hdl/calc_fwhm.vhd" "VHDL 2008"}
+  lappend Sources {"../hdl/calc_integ.vhd" "VHDL 2008"}
+  lappend Sources {"../hdl/calc_peak.vhd" "VHDL 2008"}
+  lappend Sources {"../hdl/calc_stats.vhd" "VHDL 2008"}
+  lappend Sources {"../hdl/eeprom.vhd" "VHDL 2008"}
+  lappend Sources {"../hdl/eeprom_interface.vhd" "VHDL 2008"}
+  lappend Sources {"../hdl/gen_timing_events.vhd" "VHDL 2008"}
+  lappend Sources {"../hdl/pulse_gen.vhd" "VHDL 2008"}
+  lappend Sources {"../hdl/rx_kria_data.vhd" "VHDL 2008"}
+  lappend Sources {"../hdl/sampnum.vhd" "VHDL 2008"}
+  lappend Sources {"../hdl/sfp_gtp.vhd" "VHDL 2008"}
+  lappend Sources {"../hdl/stretch.vhd" "VHDL 2008"}
+  lappend Sources {"../hdl/tx_kria_data.vhd" "VHDL 2008"}
+  
+  lappend Sources {"../hdl/gtp/gtwizard_0_clock_module.vhd" "VHDL 2008"} 
+  lappend Sources {"../hdl/gtp/gtwizard_0_common.vhd" "VHDL 2008"}  
+  lappend Sources {"../hdl/gtp/gtwizard_0_common_reset.vhd" "VHDL 2008"}
+  lappend Sources {"../hdl/gtp/gtwizard_0_cpll_railing.vhd" "VHDL 2008"}
+  lappend Sources {"../hdl/gtp/gtwizard_0_gt_usrclk_source.vhd" "VHDL 2008"}
+  lappend Sources {"../hdl/gtp/gtwizard_0_support.vhd" "VHDL 2008"}                                                                       
 
   lappend Sources {"../cstr/pins.xdc"  "XDC"} 
   lappend Sources {"../cstr/gtp.xdc"  "XDC"}     
+  #lappend Sources {"../cstr/timing.xdc"  "XDC"} 
+  #lappend Sources {"../cstr/debug.xdc"  "XDC"} 
   
   
 }
@@ -48,11 +76,20 @@ proc doOnCreate {} {
   set_property default_lib      xil_defaultlib               [current_project]
    
   
-  #source ${TclPath}/system.tcl
-  #source ${TclPath}/evr_gth.tcl
-  #source ${TclPath}/gth_artix.tcl
-  #source ${TclPath}/gth_freerun_clk.tcl
-  #source ${TclPath}/wvfm_fifo.tcl
+  source ${TclPath}/adc_fifo.tcl
+  source ${TclPath}/adc_interface_ila.tcl
+  source ${TclPath}/blk_mem_gen_0.tcl
+  source ${TclPath}/calc_ila.tcl
+  source ${TclPath}/calc_ila_debug.tcl
+  source ${TclPath}/clk_wiz_0.tcl
+  source ${TclPath}/c_shift_ram_0.tcl
+  source ${TclPath}/eeprom_ila.tcl
+  source ${TclPath}/fifo_ila.tcl
+  source ${TclPath}/gtwizard_0.tcl
+  source ${TclPath}/ila_0.tcl
+  source ${TclPath}/tpg_ila.tcl
+  source ${TclPath}/trig_vio.tcl
+  source ${TclPath}/tx_ila.tcl
   
 
   addSources "Sources" 
